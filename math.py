d67c809d9cb6b6da737f738b3f323b04f63caf22
Nikshitaburde <54346166+Nikshitaburde@users.noreply.github.com>
2022-02-19 00:23:42 +0530

    Added math.py file

---
+++ math.py
@@ -0,0 +1,13 @@
+# Add implementation
+def add(x,y):
+    pass
+
+# Substract implemetation
+def substract(x,y):
+    pass
+
+def divide(x,y):
+    pass
+# Multiply Implementation
+def multiple(x,y):
+    pass
